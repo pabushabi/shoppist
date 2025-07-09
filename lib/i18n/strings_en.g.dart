4e0b33823cd8ba2527517e64ba185bf4d02d7783
Pavel Shalimov <p.shalimov@instadev.net>
2025-07-09 19:03:38 +0300

    APP-8: android widget | bump deps | notifications

---
+++ lib/i18n/strings_en.g.dart
@@ -0,0 +1,229 @@
+///
+/// Generated file. Do not edit.
+///
+// coverage:ignore-file
+// ignore_for_file: type=lint, unused_import
+
+part of 'strings.g.dart';
+
+// Path: <root>
+typedef TranslationsEn = Translations; // ignore: unused_element
+class Translations implements BaseTranslations<AppLocale, Translations> {
+	/// Returns the current translations of the given [context].
+	///
+	/// Usage:
+	/// final t = Translations.of(context);
+	static Translations of(BuildContext context) => InheritedLocaleData.of<AppLocale, Translations>(context).translations;
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	Translations({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver, TranslationMetadata<AppLocale, Translations>? meta})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = meta ?? TranslationMetadata(
+		    locale: AppLocale.en,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <en>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	/// Access flat map
+	dynamic operator[](String key) => $meta.getTranslation(key);
+
+	late final Translations _root = this; // ignore: unused_field
+
+	Translations $copyWith({TranslationMetadata<AppLocale, Translations>? meta}) => Translations(meta: meta ?? this.$meta);
+
+	// Translations
+	String get name => 'Name';
+	String get current_count => 'Current count';
+	String get max_count => 'Max count';
+	late final TranslationsTagEn tag = TranslationsTagEn._(_root);
+	String get save => 'Save';
+	String get create_new => 'Create new item';
+	String get create => 'Create';
+	String get edit => 'Edit';
+	String get empty => 'It`s too empty here, maybe create a new one?';
+	String get deleted => 'Deleted';
+	String get delete => 'Delete';
+	String get undo => 'Undo';
+	String get add => 'Add';
+	late final TranslationsSettingsEn settings = TranslationsSettingsEn._(_root);
+	String get cancel => 'Cancel';
+	late final TranslationsColorsEn colors = TranslationsColorsEn._(_root);
+	late final TranslationsSortEn sort = TranslationsSortEn._(_root);
+	late final TranslationsPushEn push = TranslationsPushEn._(_root);
+	late final TranslationsWidgetEn widget = TranslationsWidgetEn._(_root);
+}
+
+// Path: tag
+class TranslationsTagEn {
+	TranslationsTagEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get tag => 'Tag';
+	String get add => 'Add new tag';
+	String get enter_name => 'Name:';
+	String get choose_color => 'Color:';
+	String get menu => 'Enter tag editing menu';
+	String get enter => 'Enter';
+}
+
+// Path: settings
+class TranslationsSettingsEn {
+	TranslationsSettingsEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get settings => 'Settings';
+	String get system => 'System';
+	String get language => 'Language';
+	String get theme => 'Theme';
+	String get family_code => 'Family code';
+	String get enter_code => 'Enter new family code';
+	String get note_code => 'Note: all old items you added will stay in old family and wont be transfderred to new';
+}
+
+// Path: colors
+class TranslationsColorsEn {
+	TranslationsColorsEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get red => 'Red';
+	String get pink => 'Pink';
+	String get purple => 'Purple';
+	String get deepPurple => 'Deep purple';
+	String get indigo => 'Indigo';
+	String get blue => 'Blue';
+	String get lightBlue => 'Light blue';
+	String get cyan => 'Cyan';
+	String get teal => 'Teal';
+	String get green => 'Green';
+	String get lightGreen => 'Light green';
+	String get lime => 'Lime';
+	String get yellow => 'Yellow';
+	String get amber => 'Amber';
+	String get orange => 'Orange';
+	String get deepOrange => 'Deep orange';
+	String get brown => 'Brown';
+	String get blueGrey => 'Blue grey';
+}
+
+// Path: sort
+class TranslationsSortEn {
+	TranslationsSortEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get alphabet => 'Alphabet';
+	String get type => 'Type';
+	String get none => 'None';
+}
+
+// Path: push
+class TranslationsPushEn {
+	TranslationsPushEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get turn_on => 'Turn on notifications';
+	String get is_empty => 'Notify me when stocks are almost out';
+	String get reports => 'Daily reports';
+	String get time_of_reports => 'Time of daily reports';
+	String get attention => 'Pay attention to ';
+	String get ran_out => 'ran out';
+	String get left => 'left';
+	String get productsRanOut => 'Products are running out';
+	String get daily_report => 'Daily report';
+}
+
+// Path: widget
+class TranslationsWidgetEn {
+	TranslationsWidgetEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get wowEmpty => 'Wow, it`s empty...';
+	String get goodWork => 'Good work!';
+}
+
+/// Flat map(s) containing all translations.
+/// Only for edge cases! For simple maps, use the map function of this library.
+extension on Translations {
+	dynamic _flatMapFunction(String path) {
+		switch (path) {
+			case 'name': return 'Name';
+			case 'current_count': return 'Current count';
+			case 'max_count': return 'Max count';
+			case 'tag.tag': return 'Tag';
+			case 'tag.add': return 'Add new tag';
+			case 'tag.enter_name': return 'Name:';
+			case 'tag.choose_color': return 'Color:';
+			case 'tag.menu': return 'Enter tag editing menu';
+			case 'tag.enter': return 'Enter';
+			case 'save': return 'Save';
+			case 'create_new': return 'Create new item';
+			case 'create': return 'Create';
+			case 'edit': return 'Edit';
+			case 'empty': return 'It`s too empty here, maybe create a new one?';
+			case 'deleted': return 'Deleted';
+			case 'delete': return 'Delete';
+			case 'undo': return 'Undo';
+			case 'add': return 'Add';
+			case 'settings.settings': return 'Settings';
+			case 'settings.system': return 'System';
+			case 'settings.language': return 'Language';
+			case 'settings.theme': return 'Theme';
+			case 'settings.family_code': return 'Family code';
+			case 'settings.enter_code': return 'Enter new family code';
+			case 'settings.note_code': return 'Note: all old items you added will stay in old family and wont be transfderred to new';
+			case 'cancel': return 'Cancel';
+			case 'colors.red': return 'Red';
+			case 'colors.pink': return 'Pink';
+			case 'colors.purple': return 'Purple';
+			case 'colors.deepPurple': return 'Deep purple';
+			case 'colors.indigo': return 'Indigo';
+			case 'colors.blue': return 'Blue';
+			case 'colors.lightBlue': return 'Light blue';
+			case 'colors.cyan': return 'Cyan';
+			case 'colors.teal': return 'Teal';
+			case 'colors.green': return 'Green';
+			case 'colors.lightGreen': return 'Light green';
+			case 'colors.lime': return 'Lime';
+			case 'colors.yellow': return 'Yellow';
+			case 'colors.amber': return 'Amber';
+			case 'colors.orange': return 'Orange';
+			case 'colors.deepOrange': return 'Deep orange';
+			case 'colors.brown': return 'Brown';
+			case 'colors.blueGrey': return 'Blue grey';
+			case 'sort.alphabet': return 'Alphabet';
+			case 'sort.type': return 'Type';
+			case 'sort.none': return 'None';
+			case 'push.turn_on': return 'Turn on notifications';
+			case 'push.is_empty': return 'Notify me when stocks are almost out';
+			case 'push.reports': return 'Daily reports';
+			case 'push.time_of_reports': return 'Time of daily reports';
+			case 'push.attention': return 'Pay attention to ';
+			case 'push.ran_out': return 'ran out';
+			case 'push.left': return 'left';
+			case 'push.productsRanOut': return 'Products are running out';
+			case 'push.daily_report': return 'Daily report';
+			case 'widget.wowEmpty': return 'Wow, it`s empty...';
+			case 'widget.goodWork': return 'Good work!';
+			default: return null;
+		}
+	}
+}
+
---
+++ lib/i18n/strings_ru.g.dart
@@ -0,0 +1,226 @@
+///
+/// Generated file. Do not edit.
+///
+// coverage:ignore-file
+// ignore_for_file: type=lint, unused_import
+
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:slang/generated.dart';
+import 'strings.g.dart';
+
+// Path: <root>
+class TranslationsRu implements Translations {
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	TranslationsRu({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver, TranslationMetadata<AppLocale, Translations>? meta})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = meta ?? TranslationMetadata(
+		    locale: AppLocale.ru,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <ru>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	/// Access flat map
+	@override dynamic operator[](String key) => $meta.getTranslation(key);
+
+	late final TranslationsRu _root = this; // ignore: unused_field
+
+	@override 
+	TranslationsRu $copyWith({TranslationMetadata<AppLocale, Translations>? meta}) => TranslationsRu(meta: meta ?? this.$meta);
+
+	// Translations
+	@override String get name => 'Имя';
+	@override String get current_count => 'Текущее кол-во:';
+	@override String get max_count => 'Необходимо:';
+	@override late final _TranslationsTagRu tag = _TranslationsTagRu._(_root);
+	@override String get save => 'Сохранить';
+	@override String get create_new => 'Создать новый элемент';
+	@override String get create => 'Создать';
+	@override String get edit => 'Редактировать';
+	@override String get empty => 'Что-то пустовато тут, может создать что-нибудь?';
+	@override String get deleted => 'Удален';
+	@override String get delete => 'Удалить';
+	@override String get undo => 'Отменить';
+	@override String get add => 'Добавить';
+	@override late final _TranslationsSettingsRu settings = _TranslationsSettingsRu._(_root);
+	@override String get cancel => 'Отмена';
+	@override late final _TranslationsColorsRu colors = _TranslationsColorsRu._(_root);
+	@override late final _TranslationsSortRu sort = _TranslationsSortRu._(_root);
+	@override late final _TranslationsPushRu push = _TranslationsPushRu._(_root);
+	@override late final _TranslationsWidgetRu widget = _TranslationsWidgetRu._(_root);
+}
+
+// Path: tag
+class _TranslationsTagRu implements TranslationsTagEn {
+	_TranslationsTagRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get tag => 'Тэг';
+	@override String get add => 'Добавить новый тэг';
+	@override String get enter_name => 'Имя:';
+	@override String get choose_color => 'Цвет:';
+	@override String get menu => 'Меню редактирования тэгов';
+	@override String get enter => 'Войти';
+}
+
+// Path: settings
+class _TranslationsSettingsRu implements TranslationsSettingsEn {
+	_TranslationsSettingsRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get settings => 'Настройки';
+	@override String get system => 'Системa';
+	@override String get language => 'Язык';
+	@override String get theme => 'Тема';
+	@override String get family_code => 'Код семьи';
+	@override String get enter_code => 'Введите новый код';
+	@override String get note_code => 'Внимание: все ваши старые записи останутся в старой семье и не перенесутся в новую';
+}
+
+// Path: colors
+class _TranslationsColorsRu implements TranslationsColorsEn {
+	_TranslationsColorsRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get red => 'Красный';
+	@override String get pink => 'Розовый';
+	@override String get purple => 'Фиолетовый';
+	@override String get deepPurple => 'Тёмно фиолетовый';
+	@override String get indigo => 'Индиго';
+	@override String get blue => 'Синий';
+	@override String get lightBlue => 'Светло синий';
+	@override String get cyan => 'Голубой';
+	@override String get teal => 'Бирюзовый';
+	@override String get green => 'Зелёный';
+	@override String get lightGreen => 'Светло зелёный';
+	@override String get lime => 'Лаймовый';
+	@override String get yellow => 'Жёлтый';
+	@override String get amber => 'Янтарный';
+	@override String get orange => 'Оранжевый';
+	@override String get deepOrange => 'Тёмно оранжевый';
+	@override String get brown => 'Коричнеый';
+	@override String get blueGrey => 'Серый';
+}
+
+// Path: sort
+class _TranslationsSortRu implements TranslationsSortEn {
+	_TranslationsSortRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get alphabet => 'По алфавиту';
+	@override String get type => 'По типу';
+	@override String get none => 'Нет';
+}
+
+// Path: push
+class _TranslationsPushRu implements TranslationsPushEn {
+	_TranslationsPushRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get turn_on => 'Включить уведомления';
+	@override String get is_empty => 'Уведомить, когда запасы почти кончились';
+	@override String get reports => 'Ежедневные отчеты';
+	@override String get time_of_reports => 'Время отчётов';
+	@override String get attention => 'Обратите внимание на ';
+	@override String get ran_out => 'кончилось';
+	@override String get left => 'осталось';
+	@override String get productsRanOut => 'Продукты заканчиваются';
+	@override String get daily_report => 'Ежедневный отчёт';
+}
+
+// Path: widget
+class _TranslationsWidgetRu implements TranslationsWidgetEn {
+	_TranslationsWidgetRu._(this._root);
+
+	final TranslationsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get wowEmpty => 'Ух ты, ничего нет...';
+	@override String get goodWork => 'Хорошая работа!';
+}
+
+/// Flat map(s) containing all translations.
+/// Only for edge cases! For simple maps, use the map function of this library.
+extension on TranslationsRu {
+	dynamic _flatMapFunction(String path) {
+		switch (path) {
+			case 'name': return 'Имя';
+			case 'current_count': return 'Текущее кол-во:';
+			case 'max_count': return 'Необходимо:';
+			case 'tag.tag': return 'Тэг';
+			case 'tag.add': return 'Добавить новый тэг';
+			case 'tag.enter_name': return 'Имя:';
+			case 'tag.choose_color': return 'Цвет:';
+			case 'tag.menu': return 'Меню редактирования тэгов';
+			case 'tag.enter': return 'Войти';
+			case 'save': return 'Сохранить';
+			case 'create_new': return 'Создать новый элемент';
+			case 'create': return 'Создать';
+			case 'edit': return 'Редактировать';
+			case 'empty': return 'Что-то пустовато тут, может создать что-нибудь?';
+			case 'deleted': return 'Удален';
+			case 'delete': return 'Удалить';
+			case 'undo': return 'Отменить';
+			case 'add': return 'Добавить';
+			case 'settings.settings': return 'Настройки';
+			case 'settings.system': return 'Системa';
+			case 'settings.language': return 'Язык';
+			case 'settings.theme': return 'Тема';
+			case 'settings.family_code': return 'Код семьи';
+			case 'settings.enter_code': return 'Введите новый код';
+			case 'settings.note_code': return 'Внимание: все ваши старые записи останутся в старой семье и не перенесутся в новую';
+			case 'cancel': return 'Отмена';
+			case 'colors.red': return 'Красный';
+			case 'colors.pink': return 'Розовый';
+			case 'colors.purple': return 'Фиолетовый';
+			case 'colors.deepPurple': return 'Тёмно фиолетовый';
+			case 'colors.indigo': return 'Индиго';
+			case 'colors.blue': return 'Синий';
+			case 'colors.lightBlue': return 'Светло синий';
+			case 'colors.cyan': return 'Голубой';
+			case 'colors.teal': return 'Бирюзовый';
+			case 'colors.green': return 'Зелёный';
+			case 'colors.lightGreen': return 'Светло зелёный';
+			case 'colors.lime': return 'Лаймовый';
+			case 'colors.yellow': return 'Жёлтый';
+			case 'colors.amber': return 'Янтарный';
+			case 'colors.orange': return 'Оранжевый';
+			case 'colors.deepOrange': return 'Тёмно оранжевый';
+			case 'colors.brown': return 'Коричнеый';
+			case 'colors.blueGrey': return 'Серый';
+			case 'sort.alphabet': return 'По алфавиту';
+			case 'sort.type': return 'По типу';
+			case 'sort.none': return 'Нет';
+			case 'push.turn_on': return 'Включить уведомления';
+			case 'push.is_empty': return 'Уведомить, когда запасы почти кончились';
+			case 'push.reports': return 'Ежедневные отчеты';
+			case 'push.time_of_reports': return 'Время отчётов';
+			case 'push.attention': return 'Обратите внимание на ';
+			case 'push.ran_out': return 'кончилось';
+			case 'push.left': return 'осталось';
+			case 'push.productsRanOut': return 'Продукты заканчиваются';
+			case 'push.daily_report': return 'Ежедневный отчёт';
+			case 'widget.wowEmpty': return 'Ух ты, ничего нет...';
+			case 'widget.goodWork': return 'Хорошая работа!';
+			default: return null;
+		}
+	}
+}
+
